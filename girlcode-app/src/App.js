969543afdaf24264da9e9d3085f53b99eb9e6af4
merikurisalad <116539775+merikurisalad@users.noreply.github.com>
2023-01-14 03:43:46 -0800

    Made react app
    need to figure out css, connect to backend

---
+++ girlcode-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+
+
+    <div className="App">
+      <header className="App-header">
+        <h1>Shirt</h1>
+        <button>Upload image</button>
+        <h1>Pants</h1>
+        <button>Upload image</button>
+        <h1>Skirt</h1>
+        <button>Upload image</button>
+        <h1>Image</h1>
+        <img src="https://aritzia.scene7.com/is/image/Aritzia/f22_01_a08_105414_1274_off_a?wid=1200" className="example" alt=""/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
